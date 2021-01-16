b07776f9f4fc4a510f2a6dd9be5b0340d3154b9f
chicken-developer <quynh.fullstackdev@gmail.com>
2021-01-16 10:48:29 +0700

    Add template prj with FSharp Library
    Must edit path in .fsproject first to use

---
+++ library/FSharpLib/Library.fs
@@ -0,0 +1,15 @@
+﻿module CoreGame 
+
+open UnityEngine
+
+type DemoComponent ()=
+    inherit MonoBehaviour ()
+
+    [<SerializeField>]
+    let mutable message: string = "Insert message here"
+    
+    member this.Start () =
+        Debug.Log message
+
+    member this.Update () =
+        Debug.Log message
---
+++ library/FSharpLib/build.sh
@@ -0,0 +1,3 @@
+echo "Building.............................."
+dotnet build
+echo "Done !"
